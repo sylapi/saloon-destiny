f8a853e239bbb0cc6929cd8f44ce6d6518e3229c
Patryk Rogala <p.rogala@savicki.pl>
2023-12-14 14:36:45 +0100

    business place request added

---
+++ src/Requests/BusinessPlace/GetBusinessPlacesRequest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Sylapi\Saloon\Destiny\Requests\BusinessPlace;
+
+use Saloon\Enums\Method;
+use Saloon\Http\Request;
+use Sylapi\Saloon\Destiny\DestinyConnector;
+
+class GetBusinessPlaceRequest extends Request
+{
+    protected Method $method = Method::GET;
+
+    protected ?string $connector = DestinyConnector::class;
+
+    public function resolveEndpoint(): string
+    {
+        return 'm/option/business_place';
+    }
+}
